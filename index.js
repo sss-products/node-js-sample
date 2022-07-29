aee000f834255e34e6bc2d46ca392e8687554326
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2022-07-29 16:33:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello venu')
+  response.send('Hello Prudhvi')
 })
 
 app.listen(app.get('port'), function() {
