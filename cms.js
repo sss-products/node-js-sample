86641f11331cc6de312a5fcd56c0fe6d93ee6327
ejazali1510 <91586657+ejazali1510@users.noreply.github.com>
2022-08-08 16:45:05 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello ejaz')
+  response.send('Hello ejaz from AWS&Devops teams')
 })
 
 app.listen(app.get('port'), function() {
